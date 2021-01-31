50d13ddc29d3f034bec8670ac08b95f42c1c5c52
Nurtilek2005 <nurtileks2005@gmail.com>
2021-01-31 12:05:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* ----------Задание №1---------- */
+// document.querySelector('form').addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   let value = document.querySelector('.add-task');
+//   let cross = document.createElement('span');
+//     cross.textContent = '📌'
+//     cross.className = 'del'
+//   let task = document.createElement('li');
+//     task.textContent = value.value + ' '
+//     task.className = 'task'
+//   task.append(cross);
+//   document.querySelector('.wrapper').append(task);
+
+//   this.reset();
+// });
+
+
+// $('.wrapper').on('click', 'span', function () {
+//   $(this).closest('li').remove();
+// });
+
+/* ----------Задание №2---------- */
